e991a3e36021c96f89980b2765ee516cbffb2b17
Blowez1 <55099493+Blowez1@users.noreply.github.com>
2020-10-25 16:55:47 +0300

    Added History
    Added History

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ let operators = document.getElementsByClassName("operator");
 let display = document.getElementById("calculator-value");
 let calculator = document.getElementById("calc");
 let cleaner = document.getElementById("display-clean");
+let historys = document.getElementById("element");
 
 function print() {
-    display.value = display.value + this.value; 
+    display.value = display.value + this.value;
 }
 
 [...numbers].forEach(element => {
@@ -19,11 +20,17 @@ function print() {
 calculator.addEventListener("click", calculate)
 
 function calculate() {
-    display.value = eval(display.value);
+    if (display.value != "") {
+
+        historys.innerHTML += "<li class='history-element d-inline-block'>" + display.value + " = " + eval(display.value) + "</li>";
+        display.value = eval(display.value);
+
+    }
+
 }
 
 window.onkeydown = function (buttonenter) {
-    if (buttonenter.keyCode==13) {
+    if (buttonenter.keyCode == 13) {
         document.getElementById("calc").click();
     }
 }
